0378217dc25dae1ec4d520ea1cd27eb387d16243
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-11-20 15:34:20 -0600

    connecting signup to fb. updated form css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Post from './Post'
-import { db } from './firebase';
+import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal'
-import { Button } from '@material-ui/core'
+import { Button, Input } from '@material-ui/core'
 
 function getModalStyle() {
   const top = 50;
@@ -33,22 +33,29 @@ function App() {
   const [modalStyle] = React.useState(getModalStyle)
   const [open, setOpen] = useState(false)
   const [posts, setPosts] = useState([
-    // {
-    //   userName: "srosiex",
-    //   caption: "cute pupper",
-    //   imageUrl: "https://m.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/05/16/Pictures/_1571873a-58de-11e8-b431-73159b4b09e2.jpg"
-    // },
-    // {
+
     //   userName: "lucasbub",
     //   caption: "so presh",
     //   imageUrl: "https://www.peta.org/wp-content/uploads/2015/09/iStock_000003786514_Small.jpg"
-    // },
-    // {
-    //   userName: "lunita",
-    // caption: "my favorite!",
-    // imageUrl: "https://cdn.shopify.com/s/files/1/2805/6406/products/HTB1TrO4hmfD8KJjSszhq6zIJFXat_1200x1200.jpg?v=1546478235"
-    // }
+
   ])
+  const [email, setEmail] = useState('')
+  const [username, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in ..
+        console.log(authUser)
+        setUser(authUser)
+      } else {
+        // user has logged out.. 
+        setUser(null)
+      }
+    })
+  }, []);
 
   //useEffect runs a piece of code based ona specific condition
   //runs everytime the variable changes
@@ -62,6 +69,10 @@ function App() {
     })
   }, [])
 
+    const signUp = (event) => {
+      event.preventDefault()
+      auth.createUserWithEmailAndPassword(email, password).catch((error)=> alert(error.message))
+    }
 
 
   return (
@@ -74,10 +85,33 @@ function App() {
         onClose={()=> setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <h2>I am a modal.</h2>
-
-    </div>
-      </Modal>
+          <form className="app-signup">
+          <center>        
+              <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt="header" />
+          </center>
+                <Input
+                  placeholder="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+              
+                <Input
+                  placeholder="email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  placeholder="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button type="submit" onClick={signUp}>Sign Up</Button> 
+          </form>
+       </div>
+       </Modal>
 
 
       <div className="app__header">
